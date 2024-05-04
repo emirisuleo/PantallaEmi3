6792bcda58c90ff144a852b0533f011c7c7490c5
MARILIN ELIZABETH GOMEZ <76MG35423887@campus.economicas.uba.ar>
2024-05-04 16:57:28 -0300

    Agregar archivos de proyecto.

---
+++ Menu.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace GrupoE_Protitipos
+{
+    partial class Menu
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            button6 = new Button();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(165, 43);
+            button1.Name = "button1";
+            button1.Size = new Size(308, 47);
+            button1.TabIndex = 0;
+            button1.Text = "Informar espacio disponible";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(165, 176);
+            button2.Name = "button2";
+            button2.Size = new Size(308, 47);
+            button2.TabIndex = 1;
+            button2.Text = "Procesamiento orden de preparacion";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(165, 111);
+            button3.Name = "button3";
+            button3.Size = new Size(308, 47);
+            button3.TabIndex = 2;
+            button3.Text = "Orden recepción";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(165, 238);
+            button4.Name = "button4";
+            button4.Size = new Size(308, 47);
+            button4.TabIndex = 3;
+            button4.Text = "Orden de seleccion";
+            button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            button5.Location = new Point(165, 301);
+            button5.Name = "button5";
+            button5.Size = new Size(308, 47);
+            button5.TabIndex = 4;
+            button5.Text = "Orden de entrega";
+            button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            button6.Location = new Point(165, 366);
+            button6.Name = "button6";
+            button6.Size = new Size(308, 47);
+            button6.TabIndex = 5;
+            button6.Text = "Estado de ordenes";
+            button6.UseVisualStyleBackColor = true;
+            // 
+            // Menu
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button6);
+            Controls.Add(button5);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Name = "Menu";
+            Text = "Menu Principal";
+            Load += this.Menu_Load;
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+        private Button button6;
+    }
+}
---
+++ Menu.cs
@@ -0,0 +1,15 @@
+namespace GrupoE_Protitipos
+{
+    public partial class Menu : Form
+    {
+        public Menu()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
